d328565afdc6d4d2f536739b76527382d6183ae9
A Sharma <as3426@njit.edu>
2017-07-22 10:02:34 -0400

    dueDate after invoiceDate validation added

--- index.php
+++ index.php
@@ -53,6 +53,11 @@ try {
 
         // make sure the due date is after the invoice date
 
+      if ($due_date_o < $invoice_date_o) {
+                  $message = 'The due date must come after the invoice date. Please try again.';
+		              break;
+			              }
+
         // format both dates
         $invoice_date_f = 'not implemented yet';
         $due_date_f = 'not implemented yet'; 
